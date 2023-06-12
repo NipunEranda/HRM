f7656567c126175c3a9aca286bcbeb62a5a93931
NipunEranda <namarasekara71@gmail.com>
2023-06-12 20:43:40 +0530

    changes added

--- app/functions/auth.js
+++ app/functions/auth.js
@@ -1,55 +1,105 @@
-const jwt = require('jsonwebtoken');
-const moment = require('moment');
+const jwt = require("jsonwebtoken");
+const moment = require("moment");
 const { MongoClient } = require("mongodb");
-const middy = require('middy');
+const middy = require("middy");
+const axios = require("axios");
 
 exports.systemLogin = async (event) => {
-    try{
+  let mongoClient;
+  try {
+    console.log(event.body);
 
-    }catch(e){
+    await axios
+      .post(
+        `https://login.microsoftonline.com/${process.env.AZURE_AUTH_TENENT}/oauth2/v2.0/token`,
+        {
+          client_id: process.env.AZURE_AUTH_CLIENT_ID,
+          scope: "user.read Files.Read.All offline_access",
+          code: event.body.code,
+          redirect_uri: process.env.AZURE_AUTH_REDIRECT_URI,
+          grant_type: "authorization_code",
+          client_secret: process.env.AZURE_AUTH_CLIENT_SECRET,
+          state: 12345,
+        },
+        {
+          Headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      });
 
-    }
-}
+    return {
+      status: 200,
+      response: { data: { status: "success" }, error: null },
+    };
+  } catch (e) {
+    console.log(e);
+    return { status: 500, response: { data: null, error: e } };
+  } finally {
+    if (mongoClient) mongoClient.close();
+  }
+};
 
 exports.verifyToken = function () {
-    try {
-        return ({
-            before: (handler, next) => {
-                const bearerHeader = (handler.event.headers['authorization'] != undefined) ? handler.event.headers['authorization'] : (handler.event.headers.authroization != undefined) ? handler.event.headers.authroization : null;
-                if (bearerHeader != null) {
-                    const bearer = bearerHeader.split(' ');
-                    const bearerToken = bearer[1];
-                    jwt.verify(bearerToken, process.env.SECRET);
-                    next();
-                } else {
-                    return handler.callback(null, {
-                        statusCode: 403,
-                        body: JSON.stringify({
-                            auth: 'no',
-                            message: 'Access Denied'
-                        })
-                    });
-                }
-            }
-        });
-    } catch (e) {
-        if (e.name === 'TokenExpiredError') {
-            e.message = 'Token Expired';
-        }
-        console.log(e);
-        return handler.callback(null, {
+  try {
+    return {
+      before: (handler, next) => {
+        const bearerHeader =
+          handler.event.headers["authorization"] != undefined
+            ? handler.event.headers["authorization"]
+            : handler.event.headers.authroization != undefined
+            ? handler.event.headers.authroization
+            : null;
+        if (bearerHeader != null) {
+          const bearer = bearerHeader.split(" ");
+          const bearerToken = bearer[1];
+          jwt.verify(bearerToken, process.env.SECRET);
+          next();
+        } else {
+          return handler.callback(null, {
             statusCode: 403,
-            body: JSON.stringify({})
-        });
+            body: JSON.stringify({
+              auth: "no",
+              message: "Access Denied",
+            }),
+          });
+        }
+      },
+    };
+  } catch (e) {
+    if (e.name === "TokenExpiredError") {
+      e.message = "Token Expired";
     }
-}
+    console.log(e);
+    return handler.callback(null, {
+      statusCode: 403,
+      body: JSON.stringify({}),
+    });
+  }
+};
 
 const handler = async function (event, context) {
   try {
     var result = null;
-    if (event.path == '/.netlify/functions/auth/login' && event.httpMethod == 'POST') {
-        result = await exports.systemLogin(event);
+    if (
+      event.path == "/.netlify/functions/auth/login" &&
+      event.httpMethod == "POST"
+    ) {
+      result = await exports.systemLogin(event);
+    } else if (
+      event.path == "/.netlify/functions/auth" &&
+      event.httpMethod == "GET"
+    ) {
+      console.log("test");
+      result = { status: 200, response: "test" };
     }
+    return {
+      statusCode: result ? (result.status ? result.status : 500) : 500,
+      body: JSON.stringify(result.response),
+    };
   } catch (e) {
     console.log(e);
     return {
--- app/src/views/Index.vue
+++ app/src/views/Index.vue
@@ -38,11 +38,11 @@ export default {
         "https://login.microsoftonline.com/28b2e5d6-155a-48d7-b25d-c8af3a89ecdd/oauth2/v2.0/authorize?client_id=fc7ec7b0-98c4-4b6c-aaf8-a25482d825e9&response_type=code&redirect_uri=http://localhost:8888&response_mode=query&scope=user.read Files.Read.All offline_access&state=12345&sso_reload=true";
     },
   },
-  mounted() {
+  async mounted() {
     const urlParams = new URLSearchParams(window.location.search);
     const codeString = urlParams.get("code");
     if (codeString) {
-      console.log(codeString);
+      const response = await axios.post('/.netlify/functions/auth/login', { code: codeString });
     }
   },
 };
