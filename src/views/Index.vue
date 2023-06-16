eeaca63b1e343b3fd29e936cf7b69279f5878b5e
NipunEranda <namarasekara71@gmail.com>
2023-06-16 21:11:21 +0530

    site url added

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -38,7 +38,7 @@ export default {
   methods: {
     redirectToLogin() {
       location.href =
-        "https://login.microsoftonline.com/28b2e5d6-155a-48d7-b25d-c8af3a89ecdd/oauth2/v2.0/authorize?client_id=fc7ec7b0-98c4-4b6c-aaf8-a25482d825e9&response_type=code&redirect_uri=http://localhost:8888&response_mode=query&scope=user.read Files.Read.All offline_access&state=12345&sso_reload=true";
+        "https://login.microsoftonline.com/28b2e5d6-155a-48d7-b25d-c8af3a89ecdd/oauth2/v2.0/authorize?client_id=fc7ec7b0-98c4-4b6c-aaf8-a25482d825e9&response_type=code&redirect_uri=" + process.env.VUE_APP_URL + "&response_mode=query&scope=user.read Files.Read.All offline_access&state=12345&sso_reload=true";
     },
   },
   async mounted() {
