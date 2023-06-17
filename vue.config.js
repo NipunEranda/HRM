097550c6ebea55093a678495bd0340547ee0a7a0
NipunEranda <namarasekara71@gmail.com>
2023-06-17 20:53:21 +0530

    icons updated

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,21 @@
-const { defineConfig } = require("@vue/cli-service");
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
+// module.exports = {
+//   publicPath: '',
+// }
+
+// module.exports = {
+//   configureWebpack: {
+//     devServer: {
+//       historyApiFallback: true
+//     }
+//   }
+// };
+
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-});
-
-module.exports = {
-  publicPath: '',
-}
-
-module.exports = {
-  configureWebpack: {
-    devServer: {
-      historyApiFallback: true
-    }
-  }
-};
+  transpileDependencies: true
+})
