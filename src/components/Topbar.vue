e2174708c84f49265b288efc9e52cab1d8be0f83
NipunEranda <namarasekara71@gmail.com>
2023-06-21 23:18:29 +0530

    ui bug fixes added

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -4,34 +4,35 @@
       <!-- <span
         ><img class="left-logo" src="../assets/img/banner.svg" width="190"
       /></span> -->
-      <span class="appTitle desktop pointer" @click="$router.push('/dashboard')"><span class="main">GENERAL</span> <span
-          class="sub">HRM</span></span>
+      <span class="appTitle appTitle_large pointer" @click="$router.push('/dashboard')"><span class="main">GENERAL</span>
+        <span class="sub">HRM</span></span>
       <span id="sideBarToggle" class="ps-2 pe-2 pointer" @click="toggleSideBar()"><font-awesome-icon class="icon"
           icon="fa-bars" /></span>
     </div>
     <div class="col col-4 center p-0 m-0">
-      <span class="appTitle mobile pointer" @click="$router.push('/dashboard')"><span class="main">GENERAL</span> <span
-          class="sub">HRM</span></span>
+      <span class="appTitle appTitle_small pointer" @click="$router.push('/dashboard')"><span class="main">GENERAL</span>
+        <span class="sub">HRM</span></span>
     </div>
     <div class="col col-4">
-      <div class="topBarmobileViewOptions ps-2 pe-2">
+      <div class="topBarSmallViewOptions ps-2 pe-2">
         <span>
           <font-awesome-icon class="icon" icon="fa-home" @click="$router.push('/dashboard')" /></span>
-          <span><font-awesome-icon class="icon me-3"
-            :icon="userTheme == 'light-theme' ? 'fa-moon' : 'fa-sun'" @click="setTheme" /></span>
+        <span><font-awesome-icon class="icon me-3" :icon="userTheme == 'light-theme' ? 'fa-moon' : 'fa-sun'"
+            @click="setTheme" /></span>
       </div>
-      <div class="topBarDesktopOptions">
+      <div class="topBarLargeViewOptions">
         <div class="dropdown float-end">
           <img type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"
             class="userImage float-end pointer ms-4" :src="user.avatar == undefined ? userImage : user.avatar" alt=""
-            width="35" referrerpolicy="no-referrer"/>
-          <ul class="dropdown-menu" :class="{ 'dropdown-menu-dark' : user.theme == 'dark-theme', 'dropdown-menu-light': user.theme == 'light-theme' }" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="#"><font-awesome-icon class="icon me-2"
-                  icon="fa-gear" />Preferences</a></li>
-            <li><a class="dropdown-item" href="#"><font-awesome-icon class="icon me-2"
-                  icon="fa-lock" /> Security</a></li>
-            <li><a class="dropdown-item" href="#"
-                @click="this.$store.dispatch('logout')"><font-awesome-icon class="icon me-2" icon="fa-power-off" />
+            width="35" referrerpolicy="no-referrer" />
+          <ul class="dropdown-menu"
+            :class="{ 'dropdown-menu-dark': user.theme == 'dark-theme', 'dropdown-menu-light': user.theme == 'light-theme' }"
+            aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#"><span><span><font-awesome-icon class="icon me-2"
+                      icon="fa-gear" /></span><span> Preferences</span></span></a></li>
+            <li><a class="dropdown-item" href="#"><font-awesome-icon class="icon me-2" icon="fa-lock" /> Security</a></li>
+            <li><a class="dropdown-item" href="#" @click="this.$store.dispatch('logout')"><font-awesome-icon
+                  class="icon me-2" icon="fa-power-off" />
                 Logout</a></li>
           </ul>
         </div>
