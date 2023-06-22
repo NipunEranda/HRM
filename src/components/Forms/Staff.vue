8f49cc9d7763456b2c46237612bd9d68ec248d39
NipunEranda <namarasekara71@gmail.com>
2023-06-22 22:59:21 +0530

    staff view and staff form moved to a seperate file

---
+++ src/components/Forms/Staff.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="row m-0 pb-3">
+        <div class="staff-form p-3">
+            staff
+        </div>
+    </div>
+</template>
---
+++ src/components/Views/StaffList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row m-0">
+        <div class="table-responsive p-0">
+            <table class="table table-hover table-bordered table-sm table-responsive mb-0"
+                :class="{ 'table-dark': user.theme == 'dark-theme', 'table-light': user.theme == 'light-theme' }">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Department</th>
+                        <th scope="col" class="table-row-delete-col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="pointer" v-for="employee in filteredStaff">
+                        <td v-text="employee.personal.info.fullName" @click="openEditModal"></td>
+                        <td v-text="employee.personal.info.email" @click="openEditModal"></td>
+                        <td v-text="employee.contact.address.country" @click="openEditModal"></td>
+                        <td v-text="((employee.contact.address.city == '') ? '' : (employee.contact.address.city + ', ')) + ((employee.contact.address.country == '') ? '' : employee.contact.address.country)"
+                            @click="openEditModal">
+                        </td>
+                        <td v-text="employee.work.organization.company.department" @click="openEditModal"></td>
+                        <td class="table-row-delete-col"><font-awesome-icon icon="fa-trash" /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup: () => { },
+    data() {
+        return {}
+    },
+    props: {
+        user: Object,
+        filteredStaff: Array,
+        openEditModal: { type: Function },
+    },
+}
+</script>
